90bd99ce1ffe12caea1cb135bc8b5f0925550e8c
00-status <dedypips@gmail.com>
2024-10-16 12:05:50 -0600

    Updated autoloader. Added test class. Redirecting non-API requests to root

---
+++ Lib/WeaponMaker/Service/GetWeaponService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Lib\WeaponMaker\Service;
+
+class GetWeaponService
+{
+    public function getWeapon(): string
+    {
+        return "A weapon!";
+    }
+}
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+use Lib\WeaponMaker\Service\GetWeaponService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -7,13 +8,16 @@ require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 
+$app->redirect("/{any}", "/");
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
 
 $app->get('/api/1/test', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Testing!");
+    $service = new GetWeaponService();
+
+    $response->getBody()->write($service->getWeapon());
     return $response;
 });
 
