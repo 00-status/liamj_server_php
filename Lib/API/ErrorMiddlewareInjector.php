f9cf7c340939db687a44ae388e7c9495fbe4c388
00-status <dedypips@gmail.com>
2024-11-19 10:37:57 -0600

    Moved error middleware into a separate file.

---
+++ Lib/API/ErrorMiddlewareInjector.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Lib\API;
+
+use Slim\App;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class ErrorMiddlewareInjector
+{
+    public static function injectErrorMiddleware (App $app)
+    {
+        $custom_error_handler = function (
+            Request $request,
+            \Throwable $exception,
+            bool $display_error_details,
+            bool $log_errors,
+            bool $log_error_details
+        ) use ($app): Response {
+                $response = $app
+                    ->getResponseFactory()
+                    ->createResponse();
+        
+                $response
+                    ->getBody()
+                    ->write(json_encode(['error' => $exception->getMessage()]));
+                
+                return $response->withStatus($exception->getCode());
+        };
+        
+        $error_middleware = $app->addErrorMiddleware(true, false, false);
+        $error_middleware->setDefaultErrorHandler($custom_error_handler);
+    }
+}
--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-use Lib\WeaponMaker\Domain\WeaponEffect;
-use Lib\WeaponMaker\Infrastructure\GoogleGeminiApiClient;
-use Lib\WeaponMaker\Infrastructure\WeaponEffectDbContext;
+use Lib\API\ErrorMiddlewareInjector;
 use Lib\WeaponMaker\Infrastructure\WeaponMakerRoutes;
-use Lib\WeaponMaker\Service\GenerateWeaponService;
-use Lib\WeaponMaker\Service\GetWeaponService;
-use Lib\WeaponMaker\Service\PostWeaponEffectService;
-use Lib\WeaponMaker\Service\PutWeaponEffectService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -19,26 +13,7 @@ $is_dev_env = $_ENV["SERVER_ENVIRONMENT"] === "dev";
 
 $app = AppFactory::create();
 
-$custom_error_handler = function (
-    Request $request,
-    Throwable $exception,
-    bool $display_error_details,
-    bool $log_errors,
-    bool $log_error_details
-) use ($app) {
-        $response = $app
-            ->getResponseFactory()
-            ->createResponse();
-
-        $response
-            ->getBody()
-            ->write(json_encode(['error' => $exception->getMessage()]));
-        
-        return $response->withStatus($exception->getCode());
-};
-
-$error_middleware = $app->addErrorMiddleware($is_dev_env, false, false);
-$error_middleware->setDefaultErrorHandler($custom_error_handler);
+ErrorMiddlewareInjector::injectErrorMiddleware($app);
 
 $app->group("/api/1/", function (RouteCollectorProxy $group) {
     WeaponMakerRoutes::WeaponMakerRoutes($group);
