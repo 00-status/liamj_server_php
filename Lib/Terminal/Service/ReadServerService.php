94d453dbf10a3e19d56099fcc758443b7b7561af
00-status <dedypips@gmail.com>
2024-12-12 11:57:02 -0600

    Created read server service

---
+++ Lib/Terminal/Service/ReadServerService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Lib\Terminal\Service;
+
+use DI\NotFoundException;
+use Lib\Terminal\Domain\Server;
+use Lib\Terminal\Infrastructure\TerminalServerDbContext;
+
+class ReadServerService
+{
+    public function __construct(
+        private TerminalServerDbContext $db,
+    ) {}
+
+    public function readServerService(int $server_id): Server
+    {
+        $server = $this->db->fetchServer($server_id);
+
+        if (empty($server)) {
+            throw new NotFoundException("Cannot find terminal server!", 404);
+        }
+
+        return $server;
+    }
+}
