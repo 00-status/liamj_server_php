f5cda8400df66132389bf076b005e2dc5280d41b
00-status <dedypips@gmail.com>
2024-12-13 11:01:55 -0600

    added a way to create directories

--- Lib/Terminal/Service/Directories/CreateDirectoryService.php
+++ Lib/Terminal/Service/Directories/CreateDirectoryService.php
@@ -2,7 +2,26 @@
 
 namespace Lib\Terminal\Service\Directories;
 
+use DomainException;
+use Lib\Terminal\Domain\Directory;
+use Lib\Terminal\Infrastructure\TerminalDirectoriesDbContext;
+
 class CreateDirectoryService
 {
-    
+    public function __construct(
+        private TerminalDirectoriesDbContext $db,
+    ) {}
+
+    public function createDirectory(Directory $directory): Directory
+    {
+        $new_directory_id = $this->db->insertDirectory($directory);
+
+        $directories = $this->db->fetchDirectories([$new_directory_id]);
+
+        if (empty($directories)) {
+            throw new DomainException("Directory was created, but could not be fetched", 500);
+        }
+
+        return $directories[0];
+    }
 }
