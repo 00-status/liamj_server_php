2ab5cd92bd82a2df2ad7ad129314394354b06889
00-status <dedypips@gmail.com>
2024-12-24 11:18:34 -0600

    When one deletes a directory, the associated files are also deleted

--- Lib/Terminal/Service/Directories/DeleteDirectoryService.php
+++ Lib/Terminal/Service/Directories/DeleteDirectoryService.php
@@ -3,17 +3,23 @@
 namespace Lib\Terminal\Service\Directories;
 
 use Lib\Terminal\Infrastructure\Contexts\TerminalDirectoriesDbContext;
+use Lib\Terminal\Infrastructure\Contexts\TerminalFilesDbContext;
 
 class DeleteDirectoryService
 {
     public function __construct(
         private TerminalDirectoriesDbContext $db,
+        private TerminalFilesDbContext $files_db,
     ) {}
 
     public function deleteDirectory(int $directory_id): bool
     {
-        return $this->db->deleteDirectory($directory_id);
+        $files = $this->files_db->fetchFiles($directory_id);
 
-        // TODO: Delete associated files.
+        foreach ($files as $file) {
+            $this->files_db->deleteFile($file->getId());
+        }
+
+        return $this->db->deleteDirectory($directory_id);
     }
 }
