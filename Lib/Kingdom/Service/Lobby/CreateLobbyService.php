995f7d38837aea24db5700997862f664e97274de
00-status <dedypips@gmail.com>
2026-08-19 11:11:01 -0600

    Linting create lobby service.

--- Lib/Kingdom/Service/Lobby/CreateLobbyService.php
+++ Lib/Kingdom/Service/Lobby/CreateLobbyService.php
@@ -7,34 +7,33 @@ use Lib\Kingdom\Infrastructure\Contexts\LobbyDbContext;
 
 class CreateLobbyService
 {
+    private const MAXIMUM_LOBBY_COUNT = 2;
+
     public function __construct(
         private LobbyDbContext $lobby_db,
     ) {}
 
     public function createLobby(): Lobby
     {
-        // 1. Cull expired lobbies
+        // Cull expired lobbies
         $expired_lobbies = $this->lobby_db->fetchExpiredLobbies();
         $this->lobby_db->deleteRows(array_map(fn(Lobby $lobby) => $lobby->id, $expired_lobbies));
 
-        // 2. Enforce active lobby capacity (max 2)
         $active_count = $this->lobby_db->countActiveLobbies();
-        if ($active_count >= 2) {
+        if ($active_count >= self::MAXIMUM_LOBBY_COUNT) {
             throw new \DomainException("Cannot create lobby at this time.", 403);
         }
 
-        // 3. Generate a unique 6-digit lobby code
         $lobby_code = $this->generateUniqueLobbyCode();
 
-        // 4. Set created and time_to_die (created = NOW, time_to_die = NOW + 3 hours)
-        $now = new \DateTime();
-        $created_str = $now->format('Y-m-d H:i:s');
-        $time_to_die = (clone $now)->modify('+3 hours')->format('Y-m-d H:i:s');
+        $now = new \DateTimeImmutable();
+        $created_string = $now->format('Y-m-d H:i:s');
+        $time_to_die = $now->modify('+3 hours')->format('Y-m-d H:i:s');
 
         $lobby = Lobby::fromArray([
             'lobby_code' => $lobby_code,
             'time_to_die' => $time_to_die,
-            'created' => $created_str,
+            'created' => $created_string,
         ]);
 
         $id = $this->lobby_db->insertLobby($lobby);
@@ -43,7 +42,7 @@ class CreateLobbyService
             'id' => $id,
             'lobby_code' => $lobby_code,
             'time_to_die' => $time_to_die,
-            'created' => $created_str,
+            'created' => $created_string,
         ]);
     }
 
