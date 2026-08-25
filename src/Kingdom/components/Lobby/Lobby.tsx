7dd258a1fad28168f62d991a3f70590bac5fcf52
00-status <dedypips@gmail.com>
2026-08-25 09:35:53 -0600

    Stubbed out an algorithm.

---
+++ src/Kingdom/components/Lobby/Lobby.tsx
@@ -0,0 +1,12 @@
+export const Lobby = () => {
+    // Generate Kingdom.
+    //      Allow the player to change certain params of the Kingdom to be generated: width, height, and Name.
+    //      Pressing the "Generate Kingdom" button will call the api/1/generate_kingdom API.
+    // List players.
+    //      Fetch an initial list of players from the api/1/kingdom_players endpoint.
+    //      Subscribe to socket-server "player-joined" event.
+    //          When a player joins the channel
+    //              Parse a new list of players from the event.
+    //              Update the list of players with the new list.
+    return <div></div>;
+};
