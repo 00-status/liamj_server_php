b14e58ebe503d090c9529dfcdea2bbd3fab5c02d
00-status <dedypips@gmail.com>
2026-09-01 12:15:08 -0600

    Added stats card for player

--- src/DungeonCrawler/DungeonCrawlerPage.tsx
+++ src/DungeonCrawler/DungeonCrawlerPage.tsx
@@ -1,6 +1,7 @@
 import { Page } from '../SharedComponents/Page/Page';
 
 import { MonsterStats } from './components/MonsterStats';
+import { PlayerStats } from './components/PlayerStats';
 import { Character } from './domain/types';
 
 const DungeonCrawlerPage = () => {
@@ -18,23 +19,26 @@ const DungeonCrawlerPage = () => {
         },
         modifiers: [],
     };
+    const examplePlayer: Character = {
+        name: 'Jimothy the Jacked',
+        currentHP: 20,
+        currentMP: 5,
+        stats: {
+            healthPoints: 20,
+            magicPoints: 5,
+            attack: 5,
+            magicAttack: 10,
+            defence: 2,
+            magicDefence: 2,
+        },
+        modifiers: [],
+    };
 
     return (
         <Page title="Dungeons of Galericca" routes={[]}>
             <div>
                 <MonsterStats monster={exampleMonster} />
-                <div>
-                    <div>
-                        Left Panel
-                        <h2>Player Character</h2>
-                        <div>Actions</div>
-                    </div>
-                    <div>
-                        Right Panel
-                        <div>Character Stats</div>
-                        <div>Info Log</div>
-                    </div>
-                </div>
+                <PlayerStats player={examplePlayer} />
             </div>
         </Page>
     );
---
+++ src/DungeonCrawler/components/PlayerStats.tsx
@@ -0,0 +1,39 @@
+import './player-stats.css';
+import { Character } from '../domain/types';
+import { Card } from '../../SharedComponents/Card/Card';
+
+import { CharacterStat } from './CharacterStat';
+
+type Props = {
+    player: Character;
+};
+
+export const PlayerStats = ({ player }: Props) => {
+    return (
+        <Card title={player.name}>
+            <div className="player-stats">
+                <div className="player-stats__left-panel">
+                    <div>Actions</div>
+                </div>
+                <div className="player-stats__right-panel">
+                    <div className="player-stats__stat-block">
+                        <CharacterStat
+                            label="HP"
+                            value={`${player.currentHP} / ${player.stats.healthPoints}`}
+                        />
+                        <CharacterStat
+                            label="MP"
+                            value={`${player.currentMP} / ${player.stats.magicPoints}`}
+                        />
+                        <CharacterStat label="ATK" value={player.stats.attack} />
+                        <CharacterStat label="MATK" value={player.stats.magicAttack} />
+                        <CharacterStat label="DEF" value={player.stats.defence} />
+                        <CharacterStat label="MDEF" value={player.stats.magicDefence} />
+                    </div>
+
+                    <div>Info Log</div>
+                </div>
+            </div>
+        </Card>
+    );
+};
