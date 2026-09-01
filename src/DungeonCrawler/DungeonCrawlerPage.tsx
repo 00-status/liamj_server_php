2f9555aa029ed30184cfdacfabf59c73a18fd7e4
00-status <dedypips@gmail.com>
2026-09-01 12:33:04 -0600

    Stubbed out a algorithm for attacking. Added css to main page

--- src/DungeonCrawler/DungeonCrawlerPage.tsx
+++ src/DungeonCrawler/DungeonCrawlerPage.tsx
@@ -1,44 +1,63 @@
+import { useState } from 'react';
+
 import { Page } from '../SharedComponents/Page/Page';
 
+import './dungeon-crawler-page.css';
 import { MonsterStats } from './components/MonsterStats';
 import { PlayerStats } from './components/PlayerStats';
 import { Character } from './domain/types';
 
+const exampleMonster: Character = {
+    name: 'Skeleton',
+    currentHP: 10,
+    currentMP: 5,
+    stats: {
+        healthPoints: 10,
+        magicPoints: 5,
+        attack: 5,
+        magicAttack: 10,
+        defence: 2,
+        magicDefence: 2,
+    },
+    modifiers: [],
+};
+const examplePlayer: Character = {
+    name: 'Jimothy the Jacked',
+    currentHP: 20,
+    currentMP: 5,
+    stats: {
+        healthPoints: 20,
+        magicPoints: 5,
+        attack: 5,
+        magicAttack: 10,
+        defence: 2,
+        magicDefence: 2,
+    },
+    modifiers: [],
+};
+
 const DungeonCrawlerPage = () => {
-    const exampleMonster: Character = {
-        name: 'Skeleton',
-        currentHP: 10,
-        currentMP: 5,
-        stats: {
-            healthPoints: 10,
-            magicPoints: 5,
-            attack: 5,
-            magicAttack: 10,
-            defence: 2,
-            magicDefence: 2,
-        },
-        modifiers: [],
-    };
-    const examplePlayer: Character = {
-        name: 'Jimothy the Jacked',
-        currentHP: 20,
-        currentMP: 5,
-        stats: {
-            healthPoints: 20,
-            magicPoints: 5,
-            attack: 5,
-            magicAttack: 10,
-            defence: 2,
-            magicDefence: 2,
-        },
-        modifiers: [],
+    const [currentPlayer, setCurrentPlayer] = useState<Character>(exampleMonster);
+    const [currentMonster, setCurrentMonster] = useState<Character | null>(examplePlayer);
+
+    const onPlayerAttack = () => {
+        // Decrement Monster Health based on Player Attack
+        // Add message to Log.
+        // If Monster Health is 0
+        //      Display a grantulations message.
+        //
+        // Decrement the Player's Health based on the Monster Attack.
+        // Add a message to the log.
+        // If the Player's Health is 0
+        //      Display a Game Over message.
+        setCurrentMonster(null);
     };
 
     return (
         <Page title="Dungeons of Galericca" routes={[]}>
-            <div>
-                <MonsterStats monster={exampleMonster} />
-                <PlayerStats player={examplePlayer} />
+            <div className="dungeon-crawler-page">
+                {currentMonster && <MonsterStats monster={currentMonster} />}
+                <PlayerStats player={currentPlayer} />
             </div>
         </Page>
     );
