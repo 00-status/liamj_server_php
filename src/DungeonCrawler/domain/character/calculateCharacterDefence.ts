38ebaf14ef65671ef1439d2e7ee0b7df327cdc4e
00-status <dedypips@gmail.com>
2026-09-03 14:15:26 -0600

    Fixed a bug. Added max damage reduction threshold.

--- src/DungeonCrawler/domain/character/calculateCharacterDefence.ts
+++ src/DungeonCrawler/domain/character/calculateCharacterDefence.ts
@@ -4,11 +4,9 @@ const DEFENCE_SCALING_FACTOR = 100;
 const MAGIC_DEFENCE_SCALING_FACTOR = 100;
 
 export const calculateDefence = (character: Character): number => {
-    return Math.round(DEFENCE_SCALING_FACTOR / (DEFENCE_SCALING_FACTOR + character.stats.defence));
+    return DEFENCE_SCALING_FACTOR / (DEFENCE_SCALING_FACTOR + character.stats.defence);
 };
 
 export const calculateMagicDefence = (character: Character): number => {
-    return Math.round(
-        MAGIC_DEFENCE_SCALING_FACTOR / (MAGIC_DEFENCE_SCALING_FACTOR + character.stats.defence),
-    );
+    return MAGIC_DEFENCE_SCALING_FACTOR / (MAGIC_DEFENCE_SCALING_FACTOR + character.stats.defence);
 };
