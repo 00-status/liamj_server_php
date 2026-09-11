1da28d08512a8365d7ccffccc1e365af3250ad33
00-status <dedypips@gmail.com>
2026-09-11 13:02:26 -0600

    No longer applying all modifiers to every stat

--- src/DungeonCrawler/domain/character/getCharacterStat.ts
+++ src/DungeonCrawler/domain/character/getCharacterStat.ts
@@ -3,8 +3,11 @@ import { BaseStatNames, Character, StatModifier } from '../types';
 export const getCharacterStat = (character: Character, stat: BaseStatNames): number => {
     const modifiers: StatModifier[] = [
         ...character.modifiers,
-        ...character.equipables.map((equipment) => equipment.modifier),
-    ];
+        ...character.equipables.reduce<StatModifier[]>((acc, equipment) => {
+            acc = [...acc, ...equipment.modifiers];
+            return acc;
+        }, []),
+    ].filter((modifier) => modifier.stat === stat);
 
     let statAcc = character.stats[stat];
 
